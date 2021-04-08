783e413a99fe2eb11650d83b0f1d0ae5a3256379
Iurian Simion Dorin <simi_di@yahoo.com>
2021-04-08 09:46:56 +0300

    fix

--- server.js
+++ server.js
@@ -24,13 +24,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 app.get('/', (req, res) => { res.send('it is working') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt); });
